51e3bbab80f1973c91e63b9a566c8763d3d1e1d6
msabukwaik <msabukwaik@gmail.com>
2018-01-13 15:35:25 +0200

    init

---
+++ Friends/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Friends
+//
+//  Created by Mohammed S A Kwaik on 1/13/18.
+//  Copyright © 2018 Mohammed S A Kwaik. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+	@IBOutlet weak var window: NSWindow!
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
