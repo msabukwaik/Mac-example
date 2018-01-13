c30200d8d6dc816d53528436ad791206a2cdd0cd
msabukwaik <msabukwaik@gmail.com>
2018-01-13 17:08:00 +0200

    data validation

--- Friends/MasterViewController.swift
+++ Friends/MasterViewController.swift
@@ -28,16 +28,46 @@ class MasterViewController: NSViewController {
 		age.integerValue = 1
     }
 	@IBAction func saveBtn(_ sender: Any) {
+		
+		//Checking data
+		if firstName.stringValue.isEmpty{
+			showErrorMessage(title: "First name is empty", errorMessage: "Enter a valid first name of your friend")
+			return
+		}
+		
+		if lastName.stringValue.isEmpty{
+			showErrorMessage(title: "Last name is empty", errorMessage: "Enter a valid last name of your friend")
+			return
+		}
+		
+		if age.integerValue >= 1 && age.integerValue <= 100{
+			showErrorMessage(title: "Age is invalid", errorMessage: "Enter a valid age of your friend [1-100]")
+			return
+		}
+		
+		//Save friend
 		sharedFriends.append(Friend(firstName: firstName.stringValue, lastName: lastName.stringValue, age: age.integerValue))
+		
+		
+		// Show friends
 		friendsList.stringValue = ""
 		for friend in sharedFriends {
 			friendsList.stringValue = "\(friendsList.stringValue) \n Name : \(friend.firstName) \(friend.lastName) age : \(age.stringValue) \n"
 		}
 		
+		// Reset fields
 		firstName.stringValue = ""
 		lastName.stringValue = ""
 		age.integerValue = 1
 		
+	}
+	
+	func showErrorMessage(title:String, errorMessage:String)  {
+		let errorAlert = NSAlert()
+		errorAlert.informativeText = errorMessage
+		errorAlert.messageText = title
+		errorAlert.alertStyle = .critical
+		errorAlert.runModal()
 	}
 	
 }
